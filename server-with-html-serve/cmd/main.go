28e250fdae4d9bae9cd923e64619e5341817fde1
illfate2 <illfateanother@gmail.com>
2021-01-31 20:16:58 +0300

    Add main

---
+++ server-with-html-serve/cmd/main.go
@@ -0,0 +1,14 @@
+
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/illfate2/web-services/server-with-html-serve/pkg/api"
+)
+
+func main() {
+	s := api.NewServer()
+	log.Fatal(http.ListenAndServe(":8080", s))
+}
--- server-with-html-serve/go.mod
+++ server-with-html-serve/go.mod
@@ -2,4 +2,4 @@ module github.com/illfate2/web-services/server-with-html-serve
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.1.17 // indirect
+require github.com/labstack/echo/v4 v4.1.17
