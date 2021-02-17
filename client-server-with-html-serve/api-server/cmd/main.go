c50b44acfc955bcb5fe511c44b69e2f357b3377b
illfate2 <illfateanother@gmail.com>
2021-02-17 20:29:58 +0300

    Generate museum client react app

--- client-server-with-html-serve/api-server/cmd/main.go
+++ client-server-with-html-serve/api-server/cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/kelseyhightower/envconfig"
 
@@ -38,6 +39,16 @@ func main() {
 	defer conn.Close()
 
 	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	router.Use(dataloader.LoaderMiddleware)
 
 	router.Handle("/", playground.Handler("Dataloader", "/query"))
--- client-server-with-html-serve/api-server/go.mod
+++ client-server-with-html-serve/api-server/go.mod
@@ -5,9 +5,14 @@ go 1.15
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/go-chi/chi v3.3.2+incompatible
-	github.com/go-pg/pg/v10 v10.7.5
+	github.com/go-chi/cors v1.1.1
 	github.com/jackc/pgx/v4 v4.10.1
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
