5d06569fd3496a675c279c3e34deac0c0d90fcad
illfate2 <illfateanother@gmail.com>
2021-02-24 20:35:28 +0300

    Add museim item movement

--- ssa-client-server/museum-client/src/App.js
+++ ssa-client-server/museum-client/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import MuseumSets from './MuseumSets';
-
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import MuseumSets from "./MuseumSets";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>App</h1>
+        <h1>Museum App</h1>
+        <a href="/museumItems">Museum items </a>
+        <a href="/museumSets">Museum sets </a>
+        <a href="/museumFunds">Museum funds </a>
+        <a href="/museumItemMovements">Museum movements </a>
       </header>
-
     </div>
   );
 }
---
+++ ssa-client-server/museum-client/src/EditMuseumItemMovement.js
@@ -0,0 +1,3 @@
+const EditMuseumItemMovement = () => {};
+
+export default EditMuseumItemMovement;
