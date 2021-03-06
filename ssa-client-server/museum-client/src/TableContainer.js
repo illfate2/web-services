5e735970bdd56af381fa752483022000646a681a
illfate2 <illfateanother@gmail.com>
2021-03-06 21:43:38 +0300

    Remove spaces

--- ssa-client-server/museum-client/src/TableContainer.js
+++ ssa-client-server/museum-client/src/TableContainer.js
@@ -2,20 +2,23 @@ import React from "react"
 import { useTable } from "react-table"
 
 const TableContainer = ({ columns, data }) => {
+  // Use the useTable Hook to send the columns and data to build the table
   const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
+    getTableProps, // table props from react-table
+    getTableBodyProps, // table body props from react-table
+    headerGroups, // headerGroups, if your table has groupings
+    rows, // rows for the table based on the data passed
+    prepareRow // Prepare the row (this function needs to be called for each row before getting the row props)
   } = useTable({
     columns,
-    data,
-  })
+    data
+  });
 
+  /* 
+    Render the UI for your table
+    - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
+  */
   return (
-    // If you're curious what props we get as a result of calling our getter functions (getTableProps(), getRowProps())
-    // Feel free to use console.log()  This will help you better understand how react table works underhood.
     <table {...getTableProps()}>
       <thead>
         {headerGroups.map(headerGroup => (
@@ -26,21 +29,20 @@ const TableContainer = ({ columns, data }) => {
           </tr>
         ))}
       </thead>
-
       <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
+        {rows.map((row, i) => {
+          prepareRow(row);
           return (
             <tr {...row.getRowProps()}>
               {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
               })}
             </tr>
-          )
+          );
         })}
       </tbody>
     </table>
-  )
+  );
 }
 
 export default TableContainer
